f66efec378451c3ec1910a64e9d18cc48b57525c
Rizaldy <rizaldy@tilaka.id>
2023-06-22 13:07:50 +0700

    add interface for list and detail pokemon

--- src/app/interfaces/pokemon.intrerface.ts
+++ src/app/interfaces/pokemon.intrerface.ts
@@ -1,11 +1,61 @@
 export interface IndexedPokemon {
-    name: string
-    url: string
+  name: string;
+  url: string;
 }
 
 export interface PokemonListResponse {
-    count: number
-    next: string | null
-    previous: string | null
-    results: IndexedPokemon[]
+  count: number;
+  next: string | null;
+  previous: string | null;
+  results: IndexedPokemon[];
+}
+
+export interface ListPokemon {
+  name: string;
+  url: string;
+  image: string;
+  pokedexNumber: number;
+}
+
+export interface PokemonAbility {
+  ability: {
+    name: string;
+    url: string;
+  };
+  is_hidden: boolean;
+  slot: number;
+}
+
+export interface PokemonType {
+  slot: number;
+  type: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface PokemonStat {
+  base_stat: number;
+  effort: number;
+  stat: {
+    name: string;
+    url: string;
+  };
+}
+
+export interface DetailPokemon {
+  id: number
+  name: string;
+  weight: string;
+  height: string;
+  types: PokemonType[];
+  abilities: PokemonAbility[];
+  stats: PokemonStat[];
+  sprites: {
+    other: {
+      "official-artwork": {
+        front_default: string;
+      };
+    };
+  };
 }
